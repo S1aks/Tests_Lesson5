c1d1e5377ec16f2998fa959a405e4cbdabe94480
Viacheslav Khrykin <vh78aks@gmail.com>
2022-03-30 21:42:16 +0300

    Add UI Automator test for My Device SamsungA8. Open Settings, system update menu and check updates

--- app/src/androidTest/java/com/geekbrains/tests/automator/OpenOtherAppsTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/automator/OpenOtherAppsTest.kt
@@ -4,10 +4,10 @@ import android.widget.TextView
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SdkSuppress
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiScrollable
 import androidx.test.uiautomator.UiSelector
-import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -18,49 +18,18 @@ class OpenOtherAppsTest {
     private val uiDevice: UiDevice = UiDevice.getInstance(getInstrumentation())
 
     @Test
-    fun test_OpenSettings() {
+    fun `Open_Settings_My_SamsungA8_SM-A530F`() {
+        if (!uiDevice.isScreenOn) uiDevice.wakeUp()
         uiDevice.pressHome()
-        //Открываем экран со списком установленных приложений.
-        //Обратите внимание, что на устройстве, для которого писался этот тест (Android_Emulator - Pixel_2_Q_10_api),
-        //список приложений открывается свайпом снизу вверх на главном экране.
-        //Метод swipe принимает координаты начальной и конечной точки свайпа.
-        //В нашем случае это примерно снизу экрана строго вверх. Steps указывает, в
-        //какое количество шагов мы хотим осуществить смахивание: чем выше число,
-        //тем медленнее будет осуществляться свайп
         uiDevice.swipe(500, 1500, 500, 0, 5)
-
-        //Для других устройств список установленных приложений может открываться по другому.
-        //Часто это иконка на главном экране под названием Apps.
-        //Для этого достаточно свернуть все приложения через uiDevice.pressHome() и
-        //и найти Apps на главном экране
-        //val allAppsButton: UiObject = uiDevice.findObject(UiSelector().description("Apps"))
-        //allAppsButton.clickAndWaitForNewWindow()
-        //Вполне возможно (встречается на старых устройствах), что приложения находятся на вкладке Apps (будет еще вкладка Widgets).
-        //Тогда еще найдем вкладку и выберем ее
-        //val appsTab: UiObject = uiDevice.findObject(UiSelector().text("Apps"))
-        //appsTab.click()
-
-        //Приложений, обычно, установлено столько, что кнопка может быть за границей экрана
-        //Тогда корневым контейнером будет Scrollable.
-        //Если же все приложения умещаются на одном экране, то достаточно установить scrollable(false)
-        val appViews = UiScrollable(UiSelector().scrollable(true))
-        //Если прокрутка горизонтальная (встречается на старых устройствах), нужно установить
-        // горизонтальную прокрутку (по умолчанию она вертикальная)
-        //appViews.setAsHorizontalList()
-
-        //Находим в контейнере настройки по названию иконки
-        val settingsApp = appViews
-            .getChildByText(
-                UiSelector()
-                    .className(TextView::class.java.name),
-                "Settings"
-            )
-        //Открываем
-        settingsApp.clickAndWaitForNewWindow()
-
-        //Убеждаемся, что Настройки открыты
-        val settingsValidation =
-            uiDevice.findObject(UiSelector().packageName("com.android.settings"))
-        Assert.assertTrue(settingsValidation.exists())
+        val settingsApp = uiDevice.findObject(By.text("Настройки"))
+        settingsApp.click()
+        val settingsMenuList = UiScrollable(UiSelector().scrollable(true))
+        val updateMenu = settingsMenuList.getChildByText(
+            UiSelector().className(TextView::class.java.name), "Обновление ПО"
+        )
+        updateMenu.clickAndWaitForNewWindow()
+        val checkUpdate = uiDevice.findObject(By.text("Загрузка и установка"))
+        checkUpdate.click()
     }
 }
